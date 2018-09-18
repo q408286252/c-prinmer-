d596e770d57f3c994b2d1f13145b473599c988e3
q408286252 <32360993+q408286252@users.noreply.github.com>
2018-09-19 00:57:53 +0800

    11.1

--- c++ primer 第十一章例题/++ primer 第十一章例题/main.cpp
+++ c++ primer 第十一章例题/++ primer 第十一章例题/main.cpp
@@ -14,6 +14,10 @@
 #include <algorithm>    //大部分算法整合
 #include <iterator> //各种迭代器  插入迭代器 流迭代器;方向迭代器;移动迭代器;
 #include <functional>   //有bind函数;  ref(os) 返回一个特殊可拷贝对象   cref(os)返回特殊可拷贝const对象
+#include <map>  //对应值容器
+#include <set>  //关键字容器
+#include <unordered_map>    //无序对应值容器
+#include <unordered_set>    //无序关键字容器
 
 #include "Sales_data.h"
 #include "Sales_item.h"
@@ -22,6 +26,72 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    map<string, size_t> word;
+    string s;
+    while(cin >> s){
+        s[0] = tolower(s[0]);
+        for (size_t i =0; i <= s.size(); ++i){
+            if(s[i] == '.' || s[i] == ',')
+                s = s.erase(i,1);
+        }
+        ++word[s];
+    }
+    for (auto &i : word)
+        cout << "出现" << i.first << "的次数为:" << i.second << endl;
 }
+    /*
+    map<string, size_t> word_count;
+    set<string> exclude =  {"111","222","333"};
+    string word;
+    while (cin>> word)
+        //变成只统计111 222 333之外的string次数
+        if (exclude.find(word) == exclude.end() )   //在关键字容器查找同名关键字 如果空迭代器则:
+            ++word_count[word];
+    for (const auto &w : word_count)
+        cout << w.first << "拉" << w.second << ((w.second > 1) ? "times" : "time" ) << endl;
+    */
+
+
+/*
+//11.4
+    map<string, size_t> word;
+    string s;
+    while(cin >> s){
+        s[0] = tolower(s[0]);
+        for (size_t i =0; i <= s.size(); ++i){  //我这里写!= 然后错误半天没找出来.....
+            if(s[i] == '.' || s[i] == ',')
+                s = s.erase(i,1);
+        }
+        ++word[s];
+    }
+    for (auto &i : word)
+        cout << "出现" << i.first << "的次数为:" << i.second << endl;
+
+
+
+//11.3
+map<string, size_t> word;
+    string s;
+    while(cin >> s)
+        ++word[s];
+    for (auto &i : word)
+        cout << "出现" << i.first << "的次数为:" << i.second << endl;
+
+
+
+//11.2
+    list    临时组建规模小的组织随意加入删除
+    vector  一些纪录; 纪录永远保存并且顺序固定
+    deque   排队名单只有尾添加和 首删除
+    map     纪录某些东西出现次数啊, 身份证姓名啊; 每个人员纪录啊;
+    set     保存永远不重复的东西如身份证
+
+
+
+//11.1
+    不同多了 map能同时保存两个类型值; 而vector只能保存一个
+    vector是顺序保存 而map可以顺序可以无序;
+    vector的元素值可以相同不同, 而map可以设置只能不同;
+
+
+*/
