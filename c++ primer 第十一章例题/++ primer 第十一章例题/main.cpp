48a97cb8e221a4b9f2d9e914c36450f927edb7c5
q408286252 <32360993+q408286252@users.noreply.github.com>
2018-09-21 02:30:34 +0800

    11.2.1

--- c++ primer 第十一章例题/++ primer 第十一章例题/main.cpp
+++ c++ primer 第十一章例题/++ primer 第十一章例题/main.cpp
@@ -26,91 +26,12 @@ using namespace std;
 
 int main()
 {
-
-    list<vector<string>> vecdata;
-    vector<string> name = {"b1","b2","b3","c1","c2","c3"};
-
-    for (auto str1 : name)
-        for (auto str2 : name)
-            for (auto str3 : name)
-                for (auto str4 : name)
-                    for (auto str5 : name)
-                        for (auto str6 : name)
-                            vecdata.push_back({str1,str2,str3,str4,str5,str6});
-    //beg 是每个方案
-    for (auto beg = vecdata.begin(); beg != vecdata.end(); ++beg){
-        //删除开头c1 c2 c3
-        if ((*beg)[0] != "b1" && (*beg)[0] != "b2" && (*beg)[0] != "b3" ){
-            beg = vecdata.erase(beg); //删除这个迭代器元素
-            --beg;  //上移一格
-        //结尾必须是c
-        } else if ( (*beg)[5] != "c1"
-                   &&(*beg)[5] != "c2"
-                   &&(*beg)[5] != "c3"
-                   ){
-            beg = vecdata.erase(beg);
-            --beg;
-        //删除元素重复的
-        }else if ( 1 != count(beg->begin(), beg->end(), string("b1"))
-                   || 1 != count(beg->begin(), beg->end(), "b2")
-                   || 1 != count(beg->begin(), beg->end(), "b3")
-                   || 1 != count(beg->begin(), beg->end(), "c1")
-                   || 1 != count(beg->begin(), beg->end(), "c2")
-                   || 1 != count(beg->begin(), beg->end(), "c3")
-                   ){
-            beg = vecdata.erase(beg);
-            --beg;
-        }else {
-            int i = 0;  //控制i
-            int siz = 0;
-            siz += count(beg->begin(),(beg->begin())+3, "b1");
-            siz += count(beg->begin(),(beg->begin())+3, "b2");
-            siz += count(beg->begin(),(beg->begin())+3, "b3");
-            //前三元素不能出现三次b1 b2 b3
-            if (siz ==3){
-                i += 1;
-                goto en;
-            }
-            //begg尾每个方案的元素迭代器
-            for (auto begiter = beg->begin(); begiter != (*beg).end();++begiter){
-                //遇到元素c1c2c3 时 且 之前元素没有b1b2b3
-                if ( (*begiter) == "c1" && begiter == find(beg->begin(), begiter, string("b1") ) ){
-                    i += 1;
-                    break;
-                } else if (*begiter == "c2" && begiter == find(beg->begin(), begiter, "b2") ){
-                    i += 1;
-                    break;
-                } else if (*begiter == "c3" && begiter == find(beg->begin(), begiter, "b3") ){
-                    i += 1;
-                    break;
-                }
-            }
-            en:
-            if (i != 0){
-                beg = vecdata.erase(beg);
-                --beg;
-                i = 0;
-            }
-        }
-    }
-    ofstream out("data.txt",ofstream::app);
-    //显示剩下方案:
-    for (auto i : vecdata)
-        out << i[0] << " " << i[1] << " " << i[2] << " " << i[3] << " " << i[4] << " " << i[5] << endl;
-}
-    /*
-    map<string, size_t> word;
-    string s;
-    while(cin >> s){
-        s[0] = tolower(s[0]);
-        for (size_t i =0; i <= s.size(); ++i){
-            if(s[i] == '.' || s[i] == ',')
-                s = s.erase(i,1);
-        }
-        ++word[s];
-    }
-    for (auto &i : word)
-        cout << "出现" << i.first << "的次数为:" << i.second << endl;
+    vector<int> vec = {0,1,2,3,4,5,6,1,2,3};
+    sort(vec.begin(),vec.end());
+    auto iter = unique(vec.begin(),vec.end());
+    vec.erase(iter, vec.end());
+    for (auto i : vec)
+        cout << i << endl;
 }
     /*
     map<string, size_t> word_count;
@@ -126,6 +47,31 @@ int main()
 
 
 /*
+//11.8
+    vector<int> vec = {0,1,2,3,4,5,6,1,2,3};
+    sort(vec.begin(),vec.end());
+    auto iter = unique(vec.begin(),vec.end());
+    vec.erase(iter, vec.end());
+    for (auto i : vec)
+        cout << i << endl;
+    //优点很明显 我这里要写4行命令 而set一行就行 而且vector的元素内容如果多了.删除重复将非常慢.
+
+
+//11.7      //这破书没写怎么增加map元素.... 就要我写添加元素...
+    map<string, vector<string>> demo= {{"yang",{"rui","niu"} },{"yu",{"rui","niu"} } };
+    demo.insert({"nana",{"ni","hao"}});
+
+
+//11.6
+    set和list区别太大了 第一list是链表形式且每元素右顺序排列,右编号,可重复
+    而set 不可重复,不是链表形式等.
+
+
+//11.5
+    //分情况 map在很多需要查找很多信息时候使用非常好 而set则是唯一对于很多不可重复编号是好用的.
+
+
+
 //11.4
     map<string, size_t> word;
     string s;
