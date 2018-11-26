a16b2b4f2bdcb5a27dd9f9bc9bc8746fe9201e56
q408286252 <32360993+q408286252@users.noreply.github.com>
2018-11-26 09:06:43 +0800

    13.5

--- c++ primer 第十三章例题/main.cpp
+++ c++ primer 第十三章例题/main.cpp
@@ -26,123 +26,254 @@
 
 using namespace std;
 
-
-class A{
+class StrVec{
 public:
-    A(const string &s = string()): ps(new string(s)), i(0), use(new size_t(1)) {}
-    A(const A& p): ps(p.ps), i(p.i), use(p.use){ ++*p.use; }
-    A& operator=(const A& p){
-        ++*p.use;
-        if (--*use ==0){
-            delete use;
-            delete ps;
-        }
-        ps = p.ps;
-        i = p.i;
-        use = p.use;
-        return *this;
-    };
-    ~A(){
-        if (--*use ==0){
-            delete use;
-            delete ps;
-        }
+    StrVec():elements(nullptr), first_free(nullptr),cap(nullptr){}
+    StrVec(const initializer_list<string> &s){
+        auto newdata = alloc_n_copy(s.begin(),s.end());
+        elements = newdata.first;
+        cap = first_free = newdata.second;
     }
-
-private:
-    string *ps;
-    int i;
-    size_t *use;
-};
-
-
-class Message;
-
-class Folder{
-public:
-    friend class Message;
-    void remMsg(Message *ptr){
-        set_Message.erase(ptr);
+    StrVec(const StrVec&s){            //拷贝构造函数
+        auto newdata = alloc_n_copy(s.begin(),s.end());
+        elements = newdata.first;
+        cap = first_free = newdata.second;
     }
-    void addMsg(Message *ptr){
-        set_Message.insert(ptr);
-    }
-    void save(Message &f){
-        set_Message.insert(&f);
-    }
-    void remove(Message &f){
-        set_Message.erase(&f);
-    }
-private:
-    set<Message*> set_Message;
-};
-
-class Message{
-    friend class Folder;
-    friend void swap(Message &lhs, Message &rhs);
-public:
-    explicit Message(const std::string &str = ""): contents(str) {} //初始化string值
-    //如果拷贝构造函数 string 和 F实例指针的集合 拷贝  并且 被拷贝m的集合每个实例指针调用Msg
-    Message(const Message &m):contents(m.contents), folders(m.folders) { add_to_Folders(m);}
-    //拷贝赋值运算符
-    Message& operator=(const Message &rhs){
-        //当前实例的集合 每个元素调用remMsg一次
-        remove_from_Folders();
-        contents = rhs.contents;
-        folders = rhs.folders;
-        //传参的实例集合 每个元素调用addMsg一次
-        add_to_Folders(rhs);
+    StrVec& operator=(const StrVec&s){    //拷贝赋值运算符;
+        auto newdata = alloc_n_copy(s.begin(), s.end());    //开辟新的内存创建向量
+        free();                             //删除当前
+        elements = newdata.first;
+        cap = first_free = newdata.second;
         return *this;
     }
-    ~Message(){
-        //当前实例的集合 每个元素调用 remMsg一次;
-        remove_from_Folders();
-    }
+    ~StrVec(){  free();}
 
-    void save(Folder &f){
-        folders.insert(&f);
-        f.addMsg(this);
+    void push_back(const string&s){
+        chk_n_alloc();
+        alloc.construct(first_free++,s);
     }
-    void remove(Folder &f){
-        folders.erase(&f);
-        f.remMsg(this);
+    void reserve(const int &n){ //重新构造能保存n元素的分配内存;
+        if( n > capacity()){
+            auto newcapacity = n;   //坑0元素分配是能创建的!
+            auto newdata = alloc.allocate(newcapacity); //分配新内存
+            auto dest = newdata;
+            auto elem = elements;
+            for (size_t i = 0; i != size(); ++i)
+                alloc.construct(dest++, std::move(*elem++));
+            free();
+            elements = newdata;
+            first_free = dest;
+            cap = elements + newcapacity;
+        }
     }
+    void resize(int ls,string s = string("")){
+        if (ls > size() ){
+            auto newcapacity = ls;   //坑0元素分配是能创建的!
+            auto newdata = alloc.allocate(newcapacity); //分配新内存
+            auto dest = newdata;
+            auto elem = elements;
+            for (size_t i = 0; i != size(); ++i)
+                alloc.construct(dest++, std::move(*elem++));
+            for (size_t i = 0; i != newcapacity- size(); ++i)
+                alloc.construct(dest++, s);
+            free();
+            elements = newdata;
+            first_free = dest;
+            cap = elements + newcapacity;
+        }  else if (ls < size() && ls>= 0){
+            auto p = first_free;
+            for(auto i= 0; i != size()-ls;++i)
+                alloc.destroy(--p);
+        }
+    }
+    size_t size() const{ return first_free - elements;} //当前使用元素
+    size_t capacity() const {return cap-elements;}  //最大可使用元素;
+    string* begin() const {return elements;}
+    string* end() const {return first_free;}
+
 private:
-    std::string contents;
-    std::set<Folder*> folders;
-    void add_to_Folders(const Message &m){
-        for(auto f: m.folders)
-            f->addMsg(this);
+    static std::allocator<string> alloc; //静态变量
+    string *elements;   //首
+    string *first_free; //自由
+    string *cap;        //尾后
+
+    void chk_n_alloc(){     //计算当前元素是否等于 最大分配元素数;
+        if(size() == capacity())
+            reallocate();
     }
-    void remove_from_Folders(){
-        for(auto f: folders)
-            f->remMsg(this);
+    //传参 构造一个分配内存动态向量,返回 首和尾后 指针;
+    pair<string*,string*> alloc_n_copy(const string*s1,const string*s2){
+        auto data = alloc.allocate(s2-s1);
+        return { data, uninitialized_copy(s1,s2,data) };
+    }
+    void reallocate(){  //重新分配动态内存
+        auto newcapacity = size() ? 2*size(): 1 ;   //坑0元素分配是能创建的!
+        auto newdata = alloc.allocate(newcapacity); //分配新内存
+        auto dest = newdata;
+        auto elem = elements;
+        for (size_t i = 0; i != size(); ++i)
+            alloc.construct(dest++, std::move(*elem++));
+        free();
+        elements = newdata;
+        first_free = dest;
+        cap = elements + newcapacity;
+    }
+    void free(){    //删除函数
+        if(elements){    //始指针不为空则
+            for(auto p = first_free; p != elements;)
+                alloc.destroy(--p);
+            alloc.deallocate(elements, cap-elements);
+            //另一种写法没测过 for_each(first_free,elements, [first_free](const string &s){alloc.destroy(--first_free);});
+        }
     }
 };
-
-void swap(Message &lhs, Message &rhs){
-    using std::swap;
-    for(auto f: lhs.folders)
-        f->remMsg(&lhs);
-    for(auto f : rhs.folders)
-        f->remMsg(&rhs);
-    swap(lhs.folders, rhs.folders);
-    swap(lhs.contents, rhs.contents);
-    for(auto f: lhs.folders)
-        f->addMsg(&lhs);
-    for(auto f: rhs.folders)
-        f->addMsg(&rhs);
-}
-
+std::allocator<string> StrVec::alloc;
 int main(){
-    Message m1("xingxiuyan"),m2("lindaiyu"),m3("xuebaochai");
-	Folder f1, f2, f3, f4, f5;
-	m1.save(f1);
-	f1.save(m2);
+    StrVec li;
+    StrVec l2({"a","b","c"});
+    cout << li.capacity()<< "\n" << l2.capacity() << "\n";
+    li.push_back("aa");
+    cout << li.capacity() << "\n";
+    li.reserve(10);
+    cout << li.capacity() << "\n";
+    li.resize(20,"a");
+    cout << *(li.begin() +11) << li.capacity() << "\n";
+    StrVec l3 = li;
+    cout << l3.capacity() << "\n";
 }
-
 
 /*
+
+
+
+//13.44  42 ..
+emmmm犯懒.
+
+//13.43
+for_each(first_free,elements, [first_free](const string &s){alloc.destroy(--first_free);});
+不便于理解. for循环更容易理解. 而且性能方面貌似没区别吧?
+
+
+//13.41
+写入元素= 内存分配元素时 会超内存.
+第一个元素位置永远空着
+析构元素时错误; 删除内存也会错误
+
+//13.39 -40
+class StrVec{
+public:
+    StrVec():elements(nullptr), first_free(nullptr),cap(nullptr){}
+    StrVec(const initializer_list<string> &s){
+        auto newdata = alloc_n_copy(s.begin(),s.end());
+        elements = newdata.first;
+        cap = first_free = newdata.second;
+    }
+    StrVec(const StrVec&s){            //拷贝构造函数
+        auto newdata = alloc_n_copy(s.begin(),s.end());
+        elements = newdata.first;
+        cap = first_free = newdata.second;
+    }
+    StrVec& operator=(const StrVec&s){    //拷贝赋值运算符;
+        auto newdata = alloc_n_copy(s.begin(), s.end());    //开辟新的内存创建向量
+        free();                             //删除当前
+        elements = newdata.first;
+        cap = first_free = newdata.second;
+        return *this;
+    }
+    ~StrVec(){  free();}
+
+    void push_back(const string&s){
+        chk_n_alloc();
+        alloc.construct(first_free++,s);
+    }
+    void reserve(const int &n){ //重新构造能保存n元素的分配内存;
+        if( n > capacity()){
+            auto newcapacity = n;   //坑0元素分配是能创建的!
+            auto newdata = alloc.allocate(newcapacity); //分配新内存
+            auto dest = newdata;
+            auto elem = elements;
+            for (size_t i = 0; i != size(); ++i)
+                alloc.construct(dest++, std::move(*elem++));
+            free();
+            elements = newdata;
+            first_free = dest;
+            cap = elements + newcapacity;
+        }
+    }
+    void resize(int ls,string s = string("")){
+        if (ls > size() ){
+            auto newcapacity = ls;   //坑0元素分配是能创建的!
+            auto newdata = alloc.allocate(newcapacity); //分配新内存
+            auto dest = newdata;
+            auto elem = elements;
+            for (size_t i = 0; i != size(); ++i)
+                alloc.construct(dest++, std::move(*elem++));
+            for (size_t i = 0; i != newcapacity- size(); ++i)
+                alloc.construct(dest++, s);
+            free();
+            elements = newdata;
+            first_free = dest;
+            cap = elements + newcapacity;
+        }  else if (ls < size() && ls>= 0){
+            auto p = first_free;
+            for(auto i= 0; i != size()-ls;++i)
+                alloc.destroy(--p);
+        }
+    }
+    size_t size() const{ return first_free - elements;} //当前使用元素
+    size_t capacity() const {return cap-elements;}  //最大可使用元素;
+    string* begin() const {return elements;}
+    string* end() const {return first_free;}
+
+private:
+    static std::allocator<string> alloc; //静态变量
+    string *elements;   //首
+    string *first_free; //自由
+    string *cap;        //尾后
+
+    void chk_n_alloc(){     //计算当前元素是否等于 最大分配元素数;
+        if(size() == capacity())
+            reallocate();
+    }
+    //传参 构造一个分配内存动态向量,返回 首和尾后 指针;
+    pair<string*,string*> alloc_n_copy(const string*s1,const string*s2){
+        auto data = alloc.allocate(s2-s1);
+        return { data, uninitialized_copy(s1,s2,data) };
+    }
+    void reallocate(){  //重新分配动态内存
+        auto newcapacity = size() ? 2*size(): 1 ;   //坑0元素分配是能创建的!
+        auto newdata = alloc.allocate(newcapacity); //分配新内存
+        auto dest = newdata;
+        auto elem = elements;
+        for (size_t i = 0; i != size(); ++i)
+            alloc.construct(dest++, std::move(*elem++));
+        free();
+        elements = newdata;
+        first_free = dest;
+        cap = elements + newcapacity;
+    }
+    void free(){    //删除函数
+        if(elements){    //始指针不为空则
+            for(auto p = first_free; p != elements;)
+                alloc.destroy(--p);
+            alloc.deallocate(elements, cap-elements);
+        }
+    }
+};
+std::allocator<string> StrVec::alloc;
+int main(){
+    StrVec li;
+    StrVec l2({"a","b","c"});
+    cout << li.capacity()<< "\n" << l2.capacity() << "\n";
+    li.push_back("aa");
+    cout << li.capacity() << "\n";
+    li.reserve(10);
+    cout << li.capacity() << "\n";
+    li.resize(20,"a");
+    cout << *(li.begin() +11) << li.capacity() << "\n";
+    StrVec l3 = li;
+    cout << l3.capacity() << "\n";
+}
 
 //13.33 ~13.38
 class Message;
